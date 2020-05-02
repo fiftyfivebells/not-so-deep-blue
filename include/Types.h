71a8f8a4544529e1bbb898fc666aef022baa768c
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-01 21:47:48 -0400

    Make the files start from 1 to make math easier

--- include/Types.h
+++ include/Types.h
@@ -26,7 +26,7 @@ enum Direction : int {
   SOUTH_WEST = SOUTH + WEST
 };
 
-enum File { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H };
+enum File { FILE_A = 1, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H };
 
 enum Rank { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8};
 
