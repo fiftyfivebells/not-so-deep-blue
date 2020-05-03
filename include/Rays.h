eb98e35ce7b39c8fe82a6874880448ae9be4b7ea
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-03 18:23:21 -0400

    Create header for the Rays namespace

---
+++ include/Rays.h
@@ -0,0 +1,28 @@
+#ifndef RAYS_H
+#define RAYS_H
+
+#include "Bitboard.h"
+
+
+namespace Rays {
+
+enum Direction {
+  North,
+  South,
+  East,
+  West,
+  NorthWest,
+  NorthEast,
+  SouthWest,
+  SouthEast
+};
+
+
+  extern Bitboard rays[64][8];
+
+  void init();
+
+  Bitboard getRayBoard(Direction, Square);
+}
+
+#endif
