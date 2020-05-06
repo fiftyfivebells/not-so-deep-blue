969f28e071fd67ff4a70b8819e9bca39efe059a1
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-05 20:48:13 -0400

    Implement the knight and king attacks

--- include/Attacks.h
+++ include/Attacks.h
@@ -7,10 +7,21 @@ namespace Attacks {
   extern Bitboard rookAttacks[64];
   extern Bitboard bishopAttacks[64];
   extern Bitboard queenAttacks[64];
+  extern Bitboard knightAttacks[64];
+  extern Bitboard kingAttacks[64];
 
+  void initAttacks();
   void initRookAttacks();
   void initBishopAttacks();
   void initQueenAttacks();
+  void initKnightAttacks();
+  void initKingAttacks();
+
+  Bitboard getBishopAttacks(int, Bitboard);
+  Bitboard getRookAttacks(int, Bitboard);
+  Bitboard getQueenAttacks(int, Bitboard);
+  Bitboard getKnightAttacks(int, Bitboard);
+  Bitboard getKingAttacks(int, Bitboard);
 };
 
 #endif
