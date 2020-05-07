f82c67f3157e090f79fac85f3ad315193e5c6ea9
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-07 17:34:10 -0400

    Implement getters for different piece attributes

--- src/Move.cc
+++ src/Move.cc
@@ -12,23 +12,23 @@ Move::Move(Square from, Square to, PieceType pt, unsigned int flags) {
 
 unsigned int Move::getMove() const { return move; }
 
-unsigned int Move::getToSquare() const { return 1; }
+unsigned int Move::getToSquare() const { return (move >> 15) & SQUARE_BITS; }
 
 void Move::setToSquare(Square to) {}
 
-unsigned int Move::getFromSquare() const { return 1; }
+unsigned int Move::getFromSquare() const { return (move >> 9) & SQUARE_BITS; }
 
 void Move::setFromSquare(Square from) {}
 
-unsigned int Move::getPieceType() const { return 1; }
+unsigned int Move::getPieceType() const { return move & PIECE_BITS; }
 
 void Move::setPieceType(PieceType pt) {}
 
-unsigned int Move::getCapturedPiece() const {}
+unsigned int Move::getCapturedPiece() const { return (move >> 3) & PIECE_BITS; }
 
 void Move::setCapturedPiece(PieceType pt) {}
 
-unsigned int Move::getFlag() const { return 1; }
+unsigned int Move::getFlag() const { return (move >> 21) & FLAG_BITS; }
 
 void Move::setFlag(unsigned int flag) {}
 
