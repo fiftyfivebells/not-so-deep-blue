a713dcef6a46e49c1bfdea838555c0ae1b71b114
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-08 09:00:07 -0400

    Implement getters for the different move vectors

--- src/MoveGenerator.cc
+++ src/MoveGenerator.cc
@@ -8,3 +8,6 @@ MoveGenerator::MoveGenerator(const Chessboard &cb) {
 }
 
 void MoveGenerator::generateMoves(const Chessboard &cb) {}
+std::vector<Move> MoveGenerator::getMoves() const { return moves; }
+std::vector<Move> MoveGenerator::getLegalMoves() const { return legalMoves; }
+
