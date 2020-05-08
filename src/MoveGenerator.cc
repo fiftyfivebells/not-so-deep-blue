3a54d0629af13ff72672c525cf2e674f68f2bcdb
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-08 09:33:35 -0400

    Implement function to generate rook moves

--- src/MoveGenerator.cc
+++ src/MoveGenerator.cc
@@ -11,3 +11,16 @@ void MoveGenerator::generateMoves(const Chessboard &cb) {}
 std::vector<Move> MoveGenerator::getMoves() const { return moves; }
 std::vector<Move> MoveGenerator::getLegalMoves() const { return legalMoves; }
 
+void MoveGenerator::generateRookMoves(const Chessboard &cb) {
+  Color active = cb.getActiveSide();
+  Color enemy = (active == WHITE) ? BLACK : WHITE;
+  Bitboard rooks = cb.getPiecesByType(active, ROOK);
+
+  while (rooks) {
+    Square fromSquare = popLSB(rooks);
+    Bitboard moves = cb.getAttacksFromSquare(fromSquare, active);
+
+    addMoves(cb, fromSquare, ROOK, moves, cb.getPiecesToAttack(enemy));
+  }
+}
+
