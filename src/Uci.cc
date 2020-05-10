c6cbdc09501de486439a2ef8c605358501548421
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-09 21:52:29 -0400

    Create function to communicate with UCI

--- src/Uci.cc
+++ src/Uci.cc
@@ -1,3 +1,31 @@
 #include "../include/Uci.h"
 
 Uci::Uci() {}
+
+void Uci::uciCommunication() {
+  std::string line;
+  std::string entry;
+
+  while (std::getline(std::cin, line)) {
+    std::istringstream input(line);
+    input >> entry;
+
+    if (entry == "uci")
+      engineInfo();
+    else if (entry == "setoption")
+      setOption(input);
+    else if (entry == "ucinewgame")
+      startNewGame();
+    else if (entry == "isready")
+      std::cout << "readyok" << std::endl;
+    else if (entry == "position")
+      setPosition(input);
+    else if (entry == "go")
+      go(input);
+    else if (entry == "stop")
+      break;
+    else if (entry == "quit")
+      break;
+  }
+}
+
