1fec7b113f9809a2815a1317fd43a54c275bbabc
fiftyfivebells <stephen.l.bell@gmail.com>
2020-05-11 16:13:10 -0400

    Implement the alpha-beta algorithm to search for moves

--- src/Search.cc
+++ src/Search.cc
@@ -7,4 +7,34 @@ Search::Search(Chessboard &cb) : cb(cb) {}
 
 Move Search::findBestMove() {
 
+int Search::alphaBeta(Chessboard &cb, int alpha, int beta, int depth) {
+  if (depth == 0) return quiesce(cb, alpha, beta);
+
+  int bestScore = std::numeric_limits<int>::min();
+
+  MoveGenerator mg(cb);
+  mg.generateMoves(cb);
+  mg.generateLegalMoves(cb);
+
+  if (mg.getLegalMoves().size() == 0)
+    return (cb.isColorInCheck(cb.getActiveSide()))
+               ? -(std::numeric_limits<int>::max())
+               : 0;
+
+  for (auto move : mg.getLegalMoves()) {
+    Chessboard temp = cb;
+    temp.makeMove(move);
+
+    int score = -alphaBeta(temp, -beta, -alpha, depth - 1);
+
+    if (score >= beta) return score;
+    if (score > bestScore) {
+      bestScore = score;
+      if (score > alpha) alpha = score;
+    }
+  }
+  return bestScore;
+}
+
+
 }
